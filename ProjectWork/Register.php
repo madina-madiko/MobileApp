4184002dc83876663d60933d8c92aaea73a8b6c3
madina-madiko <madina98madiko.com@gmail.com>
2017-10-11 03:30:22 +0600

    Update Register.php

--- ProjectWork/Register.php
+++ ProjectWork/Register.php
@@ -1,31 +1,58 @@
 <?php
 
-    $con = mysqli_connect("localhost", "root", "", "mobileapp");
+
+
+    $con = mysqli_connect("localhost", "root", "password", "mobileapp");
+
+
 
     
 
+
+
     $name = $_POST["name"];
 
+
+
     $username = $_POST["username"];
+
+
 
     $password = $_POST["password"];
 
 
 
-    $statement = mysqli_prepare($con, "INSERT INTO user (name, username, password) VALUES (?, ?, ?)");
+
+    $statement = mysqli_prepare($con, "INSERT INTO user (user_id, name, username, password) VALUES (?, ?, ?)");
+
+
 
     mysqli_stmt_bind_param($statement, "siss", $name, $username, $password);
 
+
+
     mysqli_stmt_execute($statement);
 
+
+
     
+
+
 
     $response = array();
 
+
+
     $response["success"] = true;  
+
+
 
     
 
+
+
     echo json_encode($response);
+
+
 
 ?>
