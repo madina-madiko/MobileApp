fb2093deef727e799575029c1ed6ff0b69fd8372
madina-madiko <madina98madiko.com@gmail.com>
2017-10-11 01:09:42 +0600

    Update Register.php

--- ProjectWork/Register.php
+++ ProjectWork/Register.php
@@ -6,17 +6,15 @@
 
     $name = $_POST["name"];
 
-    $age = $_POST["age"];
-
     $username = $_POST["username"];
 
     $password = $_POST["password"];
 
 
 
-    $statement = mysqli_prepare($con, "INSERT INTO user (name, username, age, password) VALUES (?, ?, ?, ?)");
+    $statement = mysqli_prepare($con, "INSERT INTO user (name, username, password) VALUES (?, ?, ?)");
 
-    mysqli_stmt_bind_param($statement, "siss", $name, $username, $age, $password);
+    mysqli_stmt_bind_param($statement, "siss", $name, $username, $password);
 
     mysqli_stmt_execute($statement);
 
